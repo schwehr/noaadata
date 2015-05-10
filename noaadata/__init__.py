0483248bbcabd0a8cc1a1a91c700b381f7d3847b
Kurt Schwehr <schwehr@google.com>
2015-05-11 00:44:12 +0200

    Cleanup top level makefile and noaadata/__init__.py

--- noaadata/__init__.py
+++ noaadata/__init__.py
@@ -1,15 +1,1 @@
-#!/usr/bin/env python
-'''Python module for NOAA Co-ops data.
-
-Introduction
-============
-
-FIX: write me
-
-See Also:
-  http://gpsd.berlios.de/NMEA.txt
-
-@todo: lots
-'''
-
 __version__ = '0.46'
