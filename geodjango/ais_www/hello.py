7b2598cd9f2eeedc59b7581964c3344f3e3c8e7f
Kurt Schwehr <schwehr@google.com>
2015-05-11 19:59:56 +0200

    Remove django and geodjango

--- geodjango/ais_www/hello.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-import datetime
-
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
-
